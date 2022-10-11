0187132a174f3d31aa89ccc6d9afd77bcd7ebbed
BrandanKing <79042668+BrandanKing@users.noreply.github.com>
2022-10-11 17:47:55 +0100

    Update firebase.js

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyAriVP2RJ9d5CRBkqaV1jdFhsfG0Lnasrc',
-	authDomain: 'agendum-6285e.firebaseapp.com',
-	projectId: 'agendum-6285e',
-	storageBucket: 'agendum-6285e.appspot.com',
-	messagingSenderId: '110725071912',
-	appId: '1:110725071912:web:b867559568fa716565611d',
+	apiKey: 'xxxxxxxx',
+	authDomain: 'xxxxxx',
+	projectId: 'xxxxx',
+	storageBucket: 'xxxxx',
+	messagingSenderId: 'xxxxxx',
+	appId: 'xxxxx',
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
